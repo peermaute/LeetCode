97f129886dd9d954cae2ca51a5e40de008fc5e39
Peer Maute <peer.maute@haw-hamburg.de>
2022-09-13 17:11:40 +0200

    LeetCode problem 001 - Two Sum

---
+++ src/main/java/easy/TwoSum_001.java
@@ -0,0 +1,33 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum_001 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], i);
+        }
+        for(int i = 0; i < nums.length; i++){
+            int remaining = target - nums[i];
+            if(map.containsKey(remaining) && map.get(remaining) != i){
+                return new int[]{i, map.get(remaining)};
+            }
+        }
+        return null;
+    }
+
+    public int[] twoSumOpt(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[] { numMap.get(complement), i };
+            } else {
+                numMap.put(nums[i], i);
+            }
+        }
+        return new int[] {};
+    }
+}
